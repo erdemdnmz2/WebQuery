773cf0902dc4ffa9ce1ad96536e368f13c6beb4e
erdemdnmz2 <donmezerdem2@gmail.com>
2025-10-22 00:29:50 +0300

    feat: implement rate limiting for authentication and query execution endpoints

--- query_execution/config.py
+++ query_execution/config.py
@@ -6,3 +6,4 @@ import os
 MULTIPLE_QUERY_COUNT = int(os.getenv("MULTIPLE_QUERY_COUNT", "10"))
 MAX_ROW_COUNT_WARNING = int(os.getenv("MAX_ROW_COUNT_WARNING", "10000"))
 MAX_ROW_COUNT_LIMIT = int(os.getenv("MAX_ROW_COUNT_LIMIT", "1000"))
+RATE_LIMITER = os.getenv("QUERY_RATE_LIMITER", "10/minute")
