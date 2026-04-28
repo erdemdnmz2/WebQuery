4e652642647d6b1a01879cf589782fa9ba465c0f
erdemdnmz2 <donmezerdem2@gmail.com>
2026-04-28 21:36:04 +0300

    feat: Update AI service tools and agent structure

--- ai_service/agent/agent.py
+++ ai_service/agent/agent.py
@@ -1,9 +1,11 @@
 from langchain.chat_models import init_chat_model
+from typing import List
+from tool import Tool
 
 class Agent:
     def __init__(self, model_name: str, model_provider: str, temperature: float = 0):
-        self.tools = []
-        self.messages = []
+        self.tools : List[Tool] = []
+        self.messages : str = []
         self.system_prompt = """"""
 
         self.llm = init_chat_model(
@@ -21,7 +23,7 @@ class Agent:
         messages = self.messages + [{"role": "user", "content": query}]
         return self.llm_with_tools.invoke(messages)
 
-    def switch_model(self, model_name: str, model_provider: str):
+    def switch_model(self, model_name: str, model_provider: str, temperature: int):
         self.llm = init_chat_model(
             model=model_name,
             model_provider=model_provider,
---
+++ ai_service/agent/tool.py
@@ -0,0 +1,5 @@
+
+
+class Tool():
+    def __init__(self):
+        pass
